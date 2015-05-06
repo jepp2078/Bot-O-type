f2a7be17b588c78ba4e5dc9c6ebd2cb5134314c8
bubbles100 <bubbles100j@gmail.com>
2015-05-06 22:25:07 +0200

    nertowkr thing

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour
 {
+    public GameObject[] tables;
+    public bool[] tablesOccupied;
+
+    private GameObject cameraController;
     // Use this for initialization
     void Start()
     {
@@ -33,11 +37,41 @@ public class NetworkManager : MonoBehaviour
     void OnJoinedRoom()
     {
         spawnPlayer();
+        int emptyTableID = 0;
+
+        for (emptyTableID = 0; emptyTableID < tablesOccupied.Length; emptyTableID++)
+        {
+            if (!tablesOccupied[emptyTableID])
+            {
+                cameraController.GetComponent<PhotonView>().RPC("OccupyTable", PhotonTargets.AllBufferedViaServer, emptyTableID);
+                break;
+            }
+        }
+
+        cameraController.transform.position = tables[emptyTableID].transform.position;
+        cameraController.SetActive(true);
     }
 
     void spawnPlayer()
     {
-        GameObject cameraController = PhotonNetwork.Instantiate("CameraController", new Vector3(0f, 0.5f, 0f), Quaternion.identity, 0);
-        cameraController.SetActive(true);
+        cameraController = PhotonNetwork.Instantiate("CameraController", new Vector3(0f, 0.5f, 0f), Quaternion.identity, 0);
     }
+
+    void OnLeftRoom()
+    {
+
+    }
+
+    [RPC]
+    void OccupyTable(int tableID)
+    {
+        tablesOccupied[tableID] = true;
+    }
+
+    [RPC]
+    void LeaveTable(int tableID)
+    {
+        tablesOccupied[tableID] = false;
+    }
+
 }
